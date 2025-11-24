15130e0e08932fb52cb4cd80d9216033dde2c029
Keith Sarin <keith.sarin@gmail.com>
2025-11-24 15:07:03 -0500

    se hace cambio

---
+++ proyecto integrado/controlador.js
@@ -0,0 +1,19 @@
+let boton=document.getElementById("boton")
+boton.addEventListener("click",function(evento){
+    evento.preventDefault()
+    alert("hiciste click")
+
+    let cajaUsuario=document.getElementById("cajausuario")
+    let cajaCorreo=document.getElementById("cajacorreo")
+    let cajaCiudad=document.getElementById("cajaciudad")
+    let cajaTelefono=document.getElementById("cajatelefono")
+    let cajaContraseña=document.getElementById("cajacontraseña")
+
+    let datosDelFormulario={
+        usuario:cajaUsuario.valuer,
+        correo:cajaCorreo.valuer,
+        ciudad:cajaCiudad.valuer,
+        telefono:cajaTelefono.valuer,
+        cajaContraseña:cajaContraseña.valuer,
+    }
+})
